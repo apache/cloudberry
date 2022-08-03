41f287c2b39eb49f4645cfe899d0d32031090793
Tom Lane <tgl@sss.pgh.pa.us>
2022-08-03 17:33:42 -0400

    Fix incorrect tests for SRFs in relation_can_be_sorted_early().
    Commit fac1b470a thought we could check for set-returning functions
    by testing only the top-level node in an expression tree.  This is
    wrong in itself, and to make matters worse it encouraged others
    to make the same mistake, by exporting tlist.c's special-purpose
    IS_SRF_CALL() as a widely-visible macro.  I can't find any evidence
    that anyone's taken the bait, but it was only a matter of time.
    
    Use expression_returns_set() instead, and stuff the IS_SRF_CALL()
    genie back in its bottle, this time with a warning label.  I also
    added a couple of cross-reference comments.
    
    After a fair amount of fooling around, I've despaired of making
    a robust test case that exposes the bug reliably, so no test case
    here.  (Note that the test case added by fac1b470a is itself
    broken, in that it doesn't notice if you remove the code change.
    The repro given by the bug submitter currently doesn't fail either
    in v15 or HEAD, though I suspect that may indicate an unrelated bug.)
    
    Per bug #17564 from Martijn van Oosterhout.  Back-patch to v13,
    as the faulty patch was.
    
    Discussion: https://postgr.es/m/17564-c7472c2f90ef2da3@postgresql.org

--- src/include/optimizer/optimizer.h
+++ src/include/optimizer/optimizer.h
@@ -25,11 +25,6 @@
 #include "nodes/parsenodes.h"
 #include "nodes/plannodes.h"
 
-/* Test if an expression node represents a SRF call.  Beware multiple eval! */
-#define IS_SRF_CALL(node) \
-	((IsA(node, FuncExpr) && ((FuncExpr *) (node))->funcretset) || \
-	 (IsA(node, OpExpr) && ((OpExpr *) (node))->opretset))
-
 /*
  * We don't want to include nodes/pathnodes.h here, because non-planner
  * code should generally treat PlannerInfo as an opaque typedef.
