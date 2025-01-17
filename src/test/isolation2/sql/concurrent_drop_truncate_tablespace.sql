48055e91bde238d8b0a697826da9f152a7901cb8
Hao Wu <gfphoenix78@gmail.com>
2025-01-17 04:15:21 +0000

    add test

--- src/test/isolation2/sql/concurrent_drop_truncate_tablespace.sql
+++ src/test/isolation2/sql/concurrent_drop_truncate_tablespace.sql
@@ -3,6 +3,11 @@
 -- The behavior guarantees that the table will never use a
 -- dropped or invalid tablespace.
 
+-- start_matchsubs
+-- m/ERROR:  could not create directory "pg_tblspc.*: No such file or directory/
+-- s/ERROR:  could not create directory "pg_tblspc.*: No such file or directory/ERROR:  could not create directory "pg_tblspc\/XXXX":  No such file or directory/
+-- end_matchsubs
+
 -- create a tablespace directory
 !\retcode rm -rf /tmp/concurrent_tblspace;
 !\retcode mkdir -p /tmp/concurrent_tblspace;
@@ -23,7 +28,7 @@ DROP TABLESPACE concurrent_tblspace;
 -- if  DROP TABLESPACE acquires lock first and rollback, the blocking CREATE
 -- TABLE will be successful.
 
--- suspend execution after TablespaceCreateLock is released
+-- suspend execution after tablespace lock is acquired
 SELECT gp_inject_fault('drop_tablespace_after_acquire_lock', 'suspend', dbid) FROM gp_segment_configuration WHERE content <> -1 and role='p';
 1&: DROP TABLESPACE concurrent_tblspace;
 
@@ -32,7 +37,7 @@ SELECT gp_wait_until_triggered_fault('drop_tablespace_after_acquire_lock', 1, db
    from gp_segment_configuration where content <> -1 and role='p';
 
 2&: CREATE TABLE t_in_tablespace(a int, b int) TABLESPACE concurrent_tblspace;
--- inject an error to ensure that the above DROP command will fail
+-- inject an error to ensure that the above DROP command will rollback
 SELECT gp_inject_fault('after_xlog_tblspc_drop', 'error', dbid) FROM gp_segment_configuration WHERE content <> -1 and role='p';
 SELECT gp_inject_fault('drop_tablespace_after_acquire_lock', 'reset', dbid) FROM gp_segment_configuration WHERE content <> -1 and role='p';
 -- fail
@@ -47,7 +52,7 @@ SELECT gp_inject_fault('after_xlog_tblspc_drop', 'reset', dbid) FROM gp_segment_
 -- if DROP TABLESPACE acquires lock first and going to drop, any CREATE TABLE
 -- will fail
 
--- suspend execution after TablespaceCreateLock is released
+-- suspend execution after tablespace lock is acquired
 SELECT gp_inject_fault('drop_tablespace_after_acquire_lock', 'suspend', dbid) FROM gp_segment_configuration WHERE content <> -1 and role='p';
 1&: DROP TABLESPACE concurrent_tblspace;
 
@@ -57,7 +62,7 @@ SELECT gp_wait_until_triggered_fault('drop_tablespace_after_acquire_lock', 1, db
 
 -- create a table in the same tablespace which is being dropped via a concurrent session
 2&:CREATE TABLE drop_tablespace_tbl(a int, b int) TABLESPACE concurrent_tblspace DISTRIBUTED BY (a);
--- reset the fault, drop tablespace command will not delete the data files on the tablespace
+-- reset the fault, drop tablespace command will continue
 SELECT gp_inject_fault('drop_tablespace_after_acquire_lock', 'reset', dbid) FROM gp_segment_configuration WHERE content <> -1 and role='p';
 -- success
 1<:
