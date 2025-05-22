tvesely/yugabyte-db

Refactor the syslogger pipe protocol to use a bitmask for its options
The previous protocol expected a set of matching characters to check if
a message sent was the last one or not, that changed depending on the
destination wanted:
- 't' and 'f' tracked the last message of a log sent to stderr.
- 'T' and 'F' tracked the last message of a log sent to csvlog.

This could be extended with more characters when introducing new
destinations, but using a bitmask is much more elegant.  This commit
changes the protocol so as a bitmask is used in the header of a log
chunk message sent to the syslogger, with the following options
available for now:
- log_destination as stderr.
- log_destination as csvlog.
- if a message is the last chunk of a message.

Sehrope found this issue in a patch set to introduce JSON as an option
for log_destination, but his patch made the size of the protocol header
larger.  This commit keeps the same size as the original, and adapts the
protocol as wanted.

Thanks also to Andrew Dunstan and Greg Stark for the discussion.

Author: Michael Paquier, Sehrope Sarkuni
Discussion: https://postgr.es/m/CAH7T-aqswBM6JWe4pDehi1uOiufqe06DJWaU5=X7dDLyqUExHg@mail.gmail.com

src/postgres/src/backend/utils/error/elog.c
@@ -3250,11 +3250,16 @@ write_pipe_chunks(char *data, int len, int dest)
 
 	p.proto.nuls[0] = p.proto.nuls[1] = '\0';
 	p.proto.pid = MyProcPid;
+	p.proto.flags = 0;
+	if (dest == LOG_DESTINATION_STDERR)
+		p.proto.flags |= PIPE_PROTO_DEST_STDERR;
+	else if (dest == LOG_DESTINATION_CSVLOG)
+		p.proto.flags |= PIPE_PROTO_DEST_CSVLOG;
 
 	/* write all but the last chunk */
 	while (len > PIPE_MAX_PAYLOAD)
 	{
-		p.proto.is_last = (dest == LOG_DESTINATION_CSVLOG ? 'F' : 'f');
+		/* no need to set PIPE_PROTO_IS_LAST yet */
 		p.proto.len = PIPE_MAX_PAYLOAD;
 		memcpy(p.proto.data, data, PIPE_MAX_PAYLOAD);
 		rc = write(fd, &p, PIPE_HEADER_SIZE + PIPE_MAX_PAYLOAD);
@@ -3264,7 +3269,7 @@ write_pipe_chunks(char *data, int len, int dest)
 	}
 
 	/* write the last chunk */
-	p.proto.is_last = (dest == LOG_DESTINATION_CSVLOG ? 'T' : 't');
+	p.proto.flags |= PIPE_PROTO_IS_LAST;
 	p.proto.len = len;
 	memcpy(p.proto.data, data, len);
 	rc = write(fd, &p, PIPE_HEADER_SIZE + len);

apache/cloudberry

src/backend/utils/error/elog.c
@@ -3822,7 +3822,7 @@ append_stacktrace(PipeProtoChunk *buffer, StringInfo append, void *const *stacka
 		{
 			cmdresult[stack_no][0] = '\0';   /* clear this array for later */
 			snprintf(addrtxt, sizeof(addrtxt),"%p ",stackarray[stack_no]);
-			
+
 			Assert(sizeof(cmd) > strlen(cmd));
 			strncat(cmd, addrtxt, sizeof(cmd) - strlen(cmd) - 1);
 		}
@@ -4186,7 +4186,7 @@ write_message_to_server_log(int elevel,
 	buffer.hdr.main_thid = mainthread();
 	buffer.hdr.chunk_no = 0;
 	buffer.hdr.is_last = 'f';
-	buffer.hdr.log_format = 'c';
+	buffer.hdr.flags |= PIPE_PROTO_DEST_CSVLOG;
 	buffer.hdr.log_line_number = log_line_number++;
 	buffer.hdr.is_segv_msg = 'f';
 	buffer.hdr.next = -1;
@@ -4655,14 +4655,17 @@ write_pipe_chunks(char *data, int len, int dest)
 	p.hdr.thid = mythread();
 	p.hdr.main_thid = mainthread();
 	p.hdr.chunk_no = 0;
-	p.hdr.log_format = (dest == LOG_DESTINATION_CSVLOG ? 'c' : 't');
 	p.hdr.is_segv_msg = 'f';
 	p.hdr.next = -1;
+	p.hdr.flags = 0;
+	if (dest == LOG_DESTINATION_STDERR)
+		p.hdr.flags |= PIPE_PROTO_DEST_STDERR;
+	else if (dest == LOG_DESTINATION_CSVLOG)
+		p.hdr.flags |= PIPE_PROTO_DEST_CSVLOG;
 
 	/* write all but the last chunk */
 	while (len > PIPE_MAX_PAYLOAD)
 	{
-		p.hdr.is_last = 'f';
 		p.hdr.len = PIPE_MAX_PAYLOAD;
 		memcpy(p.data, data, PIPE_MAX_PAYLOAD);
 
@@ -4679,7 +4682,7 @@ write_pipe_chunks(char *data, int len, int dest)
 	}
 
 	/* write the last chunk */
-	p.hdr.is_last = 't';
+	p.hdr.flags |= PIPE_PROTO_IS_LAST;
 	p.hdr.len = len;
 
 #ifdef USE_ASSERT_CHECKING
@@ -5251,7 +5254,7 @@ SegvBusIllName(int signal)
 	Assert(signal == SIGILL ||
 		   signal == SIGSEGV ||
 		   signal == SIGBUS);
-	
+
 	switch (signal)
 	{
 #ifdef SIGILL
@@ -5309,7 +5312,7 @@ StandardHandlerForSigillSigsegvSigbus_OnMainThread(char *processName, SIGNAL_ARG
 	buffer.hdr.main_thid = mainthread();
 	buffer.hdr.chunk_no = 0;
 	buffer.hdr.is_last = 't';
-	buffer.hdr.log_format = 'c';
+	buffer.hdr.flags |= PIPE_PROTO_DEST_CSVLOG;
 	buffer.hdr.is_segv_msg = 't';
 	buffer.hdr.log_line_number = 0;
 	buffer.hdr.next = -1;