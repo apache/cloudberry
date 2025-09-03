ae4b7f8f662c0b49f0f7b8b032d3ac4b590a94af
Hao Wu <gfphoenix78@gmail.com>
2025-09-03 05:53:29 +0000

    map compress level to acceleration for lz4

--- contrib/pax_storage/src/cpp/storage/columns/pax_compress.cc
+++ contrib/pax_storage/src/cpp/storage/columns/pax_compress.cc
@@ -235,8 +235,11 @@ size_t PaxLZ4Compressor::GetCompressBound(size_t src_len) {
 
 size_t PaxLZ4Compressor::Compress(void *dst_buff, size_t dst_cap,
                                   void *src_buff, size_t src_len, int lvl) {
+#define LZ4_MAX_ACC 65536
   // acceleration is oppsite meaning of compress level
+  // map [19, 0] to [0, LZ4_MAX_ACC]
   int acceleration = 19 - lvl;
+  acceleration = (int)(acceleration * LZ4_MAX_ACC / 20.0);
   return LZ4_compress_fast((char *)src_buff, (char *)dst_buff, src_len,
                               dst_cap, acceleration);
 }
