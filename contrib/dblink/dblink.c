d4d4eadb116615bfc117041d37bb1d3526f78ae4
reshke <reshke@double.cloud>
2025-12-12 13:43:40 +0500

    Remove PG-14 rebase artifact from dblink and fix for `dblink_get_conn` (#1484)
    * Remove PG-14 rebase artifact from dblink.
    
    dblink_connstr_check changed its return type as part of 9b75844.
    It is error-prone to rebase dblink sources since then.
    There is no need for second dblink_connstr_check, even thought it
    produces no harm.
    
    Double-check from gpdb-7:
    https://github.com/greenplum-db/gpdb-archive/blob/main/contrib/dblink/dblink.c#L290
    
    Found during PG-16 rebase review.
    
    * Fix DBLINK_GET_CONN
    
    https://github.com/apache/cloudberry/blob/9b758447dfb65dcb8024f2465c410221ded12841/contrib/dblink/dblink.c#L185
    
    use modified conn_str in dblink_get_conn, check out 8420c0f commit msg
    
    https://github.com/apache/cloudberry/blob/9b758447dfb65dcb8024f2465c410221ded12841/contrib/dblink/dblink.c#L185-L195

--- contrib/dblink/dblink.c
+++ contrib/dblink/dblink.c
@@ -202,6 +202,27 @@ dblink_get_conn(char *conname_or_str,
 		if (connstr == NULL)
 			connstr = conname_or_str;
 		connstr = dblink_connstr_check(connstr);
+		/*
+		 * We must obey fd.c's limit on non-virtual file descriptors.  Assume
+		 * that a PGconn represents one long-lived FD.  (Doing this here also
+		 * ensures that VFDs are closed if needed to make room.)
+		 */
+		if (!AcquireExternalFD())
+		{
+#ifndef WIN32					/* can't write #if within ereport() macro */
+			ereport(ERROR,
+					(errcode(ERRCODE_SQLCLIENT_UNABLE_TO_ESTABLISH_SQLCONNECTION),
+					 errmsg("could not establish connection"),
+					 errdetail("There are too many open files on the local server."),
+					 errhint("Raise the server's max_files_per_process and/or \"ulimit -n\" limits.")));
+#else
+			ereport(ERROR,
+					(errcode(ERRCODE_SQLCLIENT_UNABLE_TO_ESTABLISH_SQLCONNECTION),
+					 errmsg("could not establish connection"),
+					 errdetail("There are too many open files on the local server."),
+					 errhint("Raise the server's max_files_per_process setting.")));
+#endif
+		}
 
 		/* OK to make connection */
 		conn = libpqsrv_connect(connstr, PG_WAIT_EXTENSION);
@@ -292,6 +313,28 @@ dblink_connect(PG_FUNCTION_ARGS)
 
 	/* check password in connection string if not superuser */
 	connstr = dblink_connstr_check(connstr);
+
+	/*
+	 * We must obey fd.c's limit on non-virtual file descriptors.  Assume that
+	 * a PGconn represents one long-lived FD.  (Doing this here also ensures
+	 * that VFDs are closed if needed to make room.)
+	 */
+	if (!AcquireExternalFD())
+	{
+#ifndef WIN32					/* can't write #if within ereport() macro */
+		ereport(ERROR,
+				(errcode(ERRCODE_SQLCLIENT_UNABLE_TO_ESTABLISH_SQLCONNECTION),
+				 errmsg("could not establish connection"),
+				 errdetail("There are too many open files on the local server."),
+				 errhint("Raise the server's max_files_per_process and/or \"ulimit -n\" limits.")));
+#else
+		ereport(ERROR,
+				(errcode(ERRCODE_SQLCLIENT_UNABLE_TO_ESTABLISH_SQLCONNECTION),
+				 errmsg("could not establish connection"),
+				 errdetail("There are too many open files on the local server."),
+				 errhint("Raise the server's max_files_per_process setting.")));
+#endif
+	}
 
 	/* OK to make connection */
 	conn = libpqsrv_connect(connstr, PG_WAIT_EXTENSION);
