b4cde0902696c75d63da59ea788ec9d9288545b1
Jinbao Chen <chenjinbao1989@gmail.com>
2026-01-06 23:18:23 +0800

    Fix some answer files

--- src/test/regress/sql/session_reset.sql
+++ src/test/regress/sql/session_reset.sql
@@ -1,6 +1,6 @@
 
 set log_min_messages to ERROR;
-\getenv abs_builddir ABS_BUILDDIR
+\getenv abs_builddir PG_ABS_BUILDDIR
 \set regress_dll :abs_builddir '/regress.so'
 CREATE OR REPLACE FUNCTION gp_execute_on_server(content int, query text) returns text
 language C as :'regress_dll', 'gp_execute_on_server';
