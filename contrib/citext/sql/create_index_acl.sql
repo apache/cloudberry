050d37086328d8a421fa4e8549810a394b90a871
reshke <reshke@double.cloud>
2026-02-15 10:35:19 +0000

    Fix for ace9973867c to work in MPP

--- contrib/citext/sql/create_index_acl.sql
+++ contrib/citext/sql/create_index_acl.sql
@@ -9,6 +9,7 @@
 -- Don't override tablespaces; this version lacks allow_in_place_tablespaces.
 
 BEGIN;
+SET allow_segment_DML TO true;
 CREATE ROLE regress_minimal;
 CREATE SCHEMA s;
 CREATE EXTENSION citext SCHEMA s;
@@ -44,7 +45,7 @@ REVOKE ALL ON FUNCTION s.index_row_if FROM PUBLIC;
 GRANT EXECUTE ON FUNCTION s.index_row_if TO regress_minimal;
 -- Non-extension, non-function objects.
 CREATE COLLATION s.coll (LOCALE="C");
-CREATE TABLE s.x (y s.citext);
+CREATE TABLE s.x (y s.citext) DISTRIBUTED REPLICATED;
 ALTER TABLE s.x OWNER TO regress_minimal;
 -- Empty-table DefineIndex()
 CREATE UNIQUE INDEX u0rows ON s.x USING btree
