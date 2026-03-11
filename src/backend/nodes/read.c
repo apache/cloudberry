77c8b054a469336edd4858cbb922b6986aec17eb
Jinbao Chen <chenjinbao1989@gmail.com>
2026-03-10 21:13:55 -0400

    Fix conflicts for read.c

--- src/backend/nodes/read.c
+++ src/backend/nodes/read.c
@@ -4,13 +4,9 @@
  *	  routines to convert a string (legal ascii representation of node) back
  *	  to nodes
  *
-<<<<<<< HEAD
  * Portions Copyright (c) 2006-2008, Greenplum inc
  * Portions Copyright (c) 2012-Present VMware, Inc. or its affiliates.
- * Portions Copyright (c) 1996-2021, PostgreSQL Global Development Group
-=======
  * Portions Copyright (c) 1996-2023, PostgreSQL Global Development Group
->>>>>>> REL_16_9
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *
