gongxun0928/cloudberrydb

Enhancement: simple code optimization
1. Explicit inline functions.
2. Instead of checking the physical size of the file every time a tuple
is inserted, it is checked every 16 tuples.

performance result
```
create table t1(a int, b int, c int, d int, e text, f text,g text, h text) using pax with(compresstype
=zstd,compresslevel=5);

gpadmin=# insert into t1 select i, i+1,i+2,i+3, i::text, i::text, i::text, i::text from generate_series(1,5000000) i;
INSERT 0 5000000
Time: 6124.535 ms (00:06.125)
gpadmin=# insert into t1 select i, i+1,i+2,i+3, i::text, i::text, i::text, i::text from generate_series(1,5000000) i;
INSERT 0 5000000
Time: 5993.682 ms (00:05.994)

-- optimized with this commit
create table t1(a int, b int, c int, d int, e text, f text,g text, h text) using pax with(compresstype
=zstd,compresslevel=5);
gpadmin=# insert into t1 select i, i+1,i+2,i+3, i::text, i::text, i::text, i::text from generate_series(1,5000000) i;
INSERT 0 5000000
Time: 5713.184 ms (00:05.713)
gpadmin=# insert into t1 select i, i+1,i+2,i+3, i::text, i::text, i::text, i::text from generate_series(1,5000000) i;
INSERT 0 5000000
Time: 5430.221 ms (00:05.430)

```

contrib/pax_storage/src/cpp/access/pax_dml_state.cc
@@ -104,7 +104,7 @@ void CPaxDmlStateLocal::Reset() { cbdb::pax_memory_context = nullptr; }
 CPaxDmlStateLocal::CPaxDmlStateLocal()
     : last_oid_(InvalidOid), cb_{.func = DmlStateResetCallback, .arg = NULL} {}
 
-std::shared_ptr<CPaxDmlStateLocal::DmlStateValue>
+inline std::shared_ptr<CPaxDmlStateLocal::DmlStateValue>
 CPaxDmlStateLocal::RemoveDmlState(const Oid &oid) {
   std::shared_ptr<CPaxDmlStateLocal::DmlStateValue> value;
 
@@ -121,7 +121,7 @@ CPaxDmlStateLocal::RemoveDmlState(const Oid &oid) {
   return value;
 }
 
-std::shared_ptr<CPaxDmlStateLocal::DmlStateValue>
+inline std::shared_ptr<CPaxDmlStateLocal::DmlStateValue>
 CPaxDmlStateLocal::FindDmlState(const Oid &oid) {
   Assert(OidIsValid(oid));
 

contrib/pax_storage/src/cpp/comm/cbdb_wrappers.h
@@ -114,8 +114,6 @@ void MemoryCtxDelete(MemoryContext memory_context);
 void MemoryCtxRegisterResetCallback(MemoryContext context,
                                     MemoryContextCallback *cb);
 
-Oid RelationGetRelationId(Relation rel);
-
 static inline void *DatumToPointer(Datum d) noexcept {
   return DatumGetPointer(d);
 }
@@ -162,6 +160,10 @@ static inline Datum Float8ToDutum(float8 d) noexcept {
 
 static inline float8 DatumToFloat8(Datum d) noexcept {
   return DatumGetFloat8(d);
+}
+
+static inline Oid RelationGetRelationId(Relation rel) noexcept {
+  return RelationGetRelid(rel);
 }
 
 BpChar *BpcharInput(const char *s, size_t len, int32 atttypmod);

apache/cloudberry

contrib/pax_storage/src/cpp/access/pax_dml_state.cc
@@ -104,7 +104,7 @@ void CPaxDmlStateLocal::Reset() { cbdb::pax_memory_context = nullptr; }
 CPaxDmlStateLocal::CPaxDmlStateLocal()
     : last_oid_(InvalidOid), cb_{.func = DmlStateResetCallback, .arg = NULL} {}
 
-std::shared_ptr<CPaxDmlStateLocal::DmlStateValue>
+pg_attribute_always_inline std::shared_ptr<CPaxDmlStateLocal::DmlStateValue>
 CPaxDmlStateLocal::RemoveDmlState(const Oid &oid) {
   std::shared_ptr<CPaxDmlStateLocal::DmlStateValue> value;
 
@@ -121,7 +121,7 @@ CPaxDmlStateLocal::RemoveDmlState(const Oid &oid) {
   return value;
 }
 
-std::shared_ptr<CPaxDmlStateLocal::DmlStateValue>
+pg_attribute_always_inline std::shared_ptr<CPaxDmlStateLocal::DmlStateValue>
 CPaxDmlStateLocal::FindDmlState(const Oid &oid) {
   Assert(OidIsValid(oid));
 

contrib/pax_storage/src/cpp/comm/cbdb_wrappers.h
@@ -114,8 +114,6 @@ void MemoryCtxDelete(MemoryContext memory_context);
 void MemoryCtxRegisterResetCallback(MemoryContext context,
                                     MemoryContextCallback *cb);
 
-Oid RelationGetRelationId(Relation rel);
-
 static inline void *DatumToPointer(Datum d) noexcept {
   return DatumGetPointer(d);
 }
@@ -162,6 +160,10 @@ static inline Datum Float8ToDutum(float8 d) noexcept {
 
 static inline float8 DatumToFloat8(Datum d) noexcept {
   return DatumGetFloat8(d);
+}
+
+static pg_attribute_always_inline Oid RelationGetRelationId(Relation rel) noexcept {
+  return RelationGetRelid(rel);
 }
 
 BpChar *BpcharInput(const char *s, size_t len, int32 atttypmod);