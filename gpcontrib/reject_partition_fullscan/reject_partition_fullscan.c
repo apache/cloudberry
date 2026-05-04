ef6fba54584e5d93b04f6d8b7f9fa9393f486fa8
liu qiren <13559537330@163.com>
2026-05-04 15:06:49 +0800

    Doc: add standard ASF license headers to all new files
    Add the Apache Software Foundation license header to all newly
    created files per project contribution requirements.

--- gpcontrib/reject_partition_fullscan/reject_partition_fullscan.c
+++ gpcontrib/reject_partition_fullscan/reject_partition_fullscan.c
@@ -1,6 +1,24 @@
 /*-------------------------------------------------------------------------
  *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
  * reject_partition_fullscan.c
+ *
  *		Extension to reject queries that scan all partitions of a
  *		partitioned table without effective partition pruning.
  *
@@ -32,8 +50,8 @@
  *   reject_partition_fullscan   (bool, default true)
  *   partition_fullscan_threshold (int, default 0)
  *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.
+ * IDENTIFICATION
+ *	  gpcontrib/reject_partition_fullscan/reject_partition_fullscan.c
  *
  *-------------------------------------------------------------------------
  */
