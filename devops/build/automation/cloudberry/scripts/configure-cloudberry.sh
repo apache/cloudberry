aa36c3505cab95023f3937375cacc73f21f0a360
liushengsong <lss602726449@gmail.com>
2026-05-14 14:48:28 +0800

    Remove duplicate --with-zstd options in configure script

--- devops/build/automation/cloudberry/scripts/configure-cloudberry.sh
+++ devops/build/automation/cloudberry/scripts/configure-cloudberry.sh
@@ -168,7 +168,6 @@ execute_cmd ./configure --prefix=${BUILD_DESTINATION} \
             --with-ldap \
             --with-libxml \
             --with-lz4 \
-            --with-zstd \
             --with-openssl \
             --with-zstd \
             --with-pam \
@@ -178,7 +177,6 @@ execute_cmd ./configure --prefix=${BUILD_DESTINATION} \
             --with-pythonsrc-ext \
             --with-ssl=openssl \
             --with-openssl \
-            --with-zstd \
             --with-uuid=e2fs \
             ${CONFIGURE_MDBLOCALES_OPTS} \
             --with-includes=/usr/local/xerces-c/include \
