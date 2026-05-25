x4m/gpdb

Add special case - mdb_admin group

src/test/regress/sql/resource_group.sql
@@ -25,3 +25,27 @@ CREATE RESOURCE GROUP rg_dump_test3 WITH (concurrency=2, cpu_rate_limit=5, memor
 CREATE ROLE role_dump_test1 RESOURCE GROUP rg_dump_test1;
 CREATE ROLE role_dump_test2 RESOURCE GROUP rg_dump_test2;
 CREATE ROLE role_dump_test3 RESOURCE GROUP rg_dump_test3;
+
+CREATE ROLE mdb_admin RESOURCE GROUP rg_dump_test1;
+CREATE ROLE not_mdb_admin RESOURCE GROUP rg_dump_test1;
+
+SET ROLE mdb_admin;
+
+CREATE RESOURCE GROUP mdb_admin_test1 WITH (concurrency=2, cpu_rate_limit=5, memory_limit=5);
+CREATE RESOURCE GROUP mdb_admin_test2 WITH (concurrency=2, cpu_rate_limit=5, memory_limit=5);
+ALTER RESOURCE GROUP mdb_admin_test1 SET cpu_rate_limit 2;
+DROP RESOURCE GROUP mdb_admin_test1;
+ALTER RESOURCE GROUP admin_group SET cpu_rate_limit 2;
+DROP RESOURCE GROUP admin_group;
+
+SET ROLE not_mdb_admin;
+
+CREATE RESOURCE GROUP mdb_admin_test1 WITH (concurrency=2, cpu_rate_limit=5, memory_limit=5);
+ALTER RESOURCE GROUP mdb_admin_test2 SET cpu_rate_limit 2;
+DROP RESOURCE GROUP mdb_admin_test2;
+DROP RESOURCE GROUP admin_group;
+
+RESET ROLE;
+DROP RESOURCE GROUP mdb_admin_test2;
+DROP ROLE mdb_admin;
+DROP ROLE not_mdb_admin;

apache/cloudberry

(cherry picked from commit 3ac99962ad23ec95bb9ef8967a4226e35cf97cc7)

src/test/regress/sql/resource_group.sql
@@ -25,3 +25,27 @@ CREATE RESOURCE GROUP rg_dump_test3 WITH (concurrency=2, cpu_max_percent=5);
 CREATE ROLE role_dump_test1 RESOURCE GROUP rg_dump_test1;
 CREATE ROLE role_dump_test2 RESOURCE GROUP rg_dump_test2;
 CREATE ROLE role_dump_test3 RESOURCE GROUP rg_dump_test3;
+
+CREATE ROLE mdb_admin RESOURCE GROUP rg_dump_test1;
+CREATE ROLE not_mdb_admin RESOURCE GROUP rg_dump_test1;
+
+SET ROLE mdb_admin;
+
+CREATE RESOURCE GROUP mdb_admin_test1 WITH (concurrency=2, cpu_max_percent=5, memory_quota=5);
+CREATE RESOURCE GROUP mdb_admin_test2 WITH (concurrency=2, cpu_max_percent=5, memory_quota=5);
+ALTER RESOURCE GROUP mdb_admin_test1 SET cpu_max_percent 2;
+DROP RESOURCE GROUP mdb_admin_test1;
+ALTER RESOURCE GROUP admin_group SET cpu_max_percent 2;
+DROP RESOURCE GROUP admin_group;
+
+SET ROLE not_mdb_admin;
+
+CREATE RESOURCE GROUP mdb_admin_test1 WITH (concurrency=2, cpu_max_percent=5, memory_quota=5);
+ALTER RESOURCE GROUP mdb_admin_test2 SET cpu_max_percent 2;
+DROP RESOURCE GROUP mdb_admin_test2;
+DROP RESOURCE GROUP admin_group;
+
+RESET ROLE;
+DROP RESOURCE GROUP mdb_admin_test2;
+DROP ROLE mdb_admin;
+DROP ROLE not_mdb_admin;